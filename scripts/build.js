a97cd37f79ff71f27b901e89461c4a2928d24e8f
Tom Theisel <tom.theisel@gmail.com>
2025-06-11 09:25:00 +1000

    chore(build): streamline build process
    Unify build process using standalone Node scripts

---
+++ scripts/build.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env zx
+
+// Run pre-processing script for the build
+echo('Running pre-processing script for the build...');
+await $`zx ./scripts/build-preprocess.js`;
+// Build the core library
+echo('Building the core library...');
+await $`pnpm --filter @yoot/yoot build`;
+// Build all adapter libraries in parallel
+echo('Building all adapter libraries in parallel...');
+await $`pnpm --filter './packages/adapters/**' --parallel -r build`;
+// Run post-processing script for the build
+echo('Running post-processing script for the build...');
+await $`zx ./scripts/build-postprocess.js`;
